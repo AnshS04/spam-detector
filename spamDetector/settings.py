3ec7a47735bbc6920130c3949cb2eba9c6a19b10
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2024-09-26 13:40:47 +0530

    removed secret key

--- spamDetector/settings.py
+++ spamDetector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tv8fmddj0+v79aub3x30l0wwabf$i-a(&xsyv=-mr3-z^=qbn)'
+SECRET_KEY = os.getenv("SETTINGS_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
